8536bf6d2210faa8f6d0c3fd722618ee4258b2e0
Justin Herrick <justin@theironyard.com>
2015-05-13 19:47:22 -0500

    updating war

--- week2/day3/war/bin/war.rb
+++ week2/day3/war/bin/war.rb
@@ -3,3 +3,9 @@ require_relative '../lib/game'
 game = Game.new
 
 game.start
+
+#To more easily run my game I have created this war.rb file
+#First it loads up the game file
+#Then it creates a new instance of our Game class.
+#Then it starts the game.
+#
