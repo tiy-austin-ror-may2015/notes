ea58c62e6da445a13a35cc61579a85697b88faf7
Justin Herrick <justin@theironyard.com>
2015-05-19 16:50:24 -0500

    Adding more comments and fixing syntax errors.

--- week3/day2/irb-history.rb
+++ week3/day2/irb-history.rb
@@ -32,16 +32,26 @@ first_row["What got shipped"]
 log_data.map do |row|
   row["Money we made"]
 end
-
+# do/end version and {} version are identical, but {}
+# are easier when so short.
 monies = log_data.map { |row| row["Money we made"] }
+# Getting all the money in the csv table
+
+
 total_money = monies.map { |money| money.to_i }.reduce(:+)
 total_money = monies.map { |money| money.to_i }.inject(:+)
+#Getting the sum of all the money in the csv table
+# Inject and reduce are identical in behavior.
 
 total_money = log_data.map { |row| row["Money we made"].to_i }.reduce(:+)
+#Everythign in one line
 
-pizza_rows = log_data.select { |row| row["What got shipped"] == "Pizza" }
-pizza_rows.map { |row| row["Money we made"].to_i }.reduce(:+)
+pizza_rows  = log_data.select { |row| row["What got shipped"] == "Pizza" }
+pizza_total = pizza_rows.map { |row| row["Money we made"].to_i }.reduce(:+)
+#Get the sum of all money earned from shipping pizza
 
+#Lets write a method that will sum the money earned from any
+#collection of csv rows.
 def total(rows)
   rows.map { |row| row["Money we made"].to_i }.reduce(:+)
 end
@@ -50,23 +60,25 @@ total(log_data)
 total(pizza_rows)
 total(log_data.select { |row| row["What got shipped"] == "Whiskey" })
 
+#We can open a new file and write to it any any time
+#This demonstrates adding 3 rows to a csv file with no headers
 CSV.open('names.csv', 'w+') do |csv|
   csv << ['john doe']
   csv << ['jane smith']
   csv << ['foo bar']
-name_csv = _
-name_csv
-name_csv.save
+end
 
 CSV.open('names.csv', 'w') do |csv|
-  csv << ["name", "age", "course"]
+  csv << ["name", "age", "course"] #this is the header row
   csv << ["jane doe", "99", "ruby"]
   csv << ["john smith", "99", "js"]
 end
+#This demonstates adding 2 rows to a csv add giving it a header row
 
-name_csv = CSV.read('names.csv', headers:true)
+name_csv = CSV.read('names.csv', headers:true) #loading a csv with headers
 name_csv.each do |row|
   puts row
 end
 
 name_csv.select { |row| row["course"] == 'ruby' }
+#grab all rows from our csv where the course is ruby
