b38717979657f7a4f696343404ecf2a604c72889
Justin Herrick <justin@theironyard.com>
2015-06-04 16:18:35 -0500

    Adding day 4 notes

--- week5/day3/student_registry/config/routes.rb
+++ week5/day3/student_registry/config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get '/students/highest_gpa', to: 'students#highest_gpa'
 
-  get('/students/highest_gpa', {to: 'students#highest_gpa'})
-
-  get('/students/:id', {to: 'students#show' })
-  put '/students/:id', to: 'students#update'
+  get    'students/:id', to: 'students#show'
+  put    'students/:id', to: 'students#update'
   delete 'students/:id', to: 'students#destroy'
-  get('/students', { to: 'students#index'  })
-  post('/students', { to: 'students#create' })
+
+  get    'students',  to: 'students#index'
+  post   'students',  to: 'students#create'
 
   root 'students#index'
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 end
---
+++ week5/day4/readme.md
@@ -0,0 +1,18 @@
+- Review homework
+- Go over heroku
+  - `heroku create`
+  - `git push heroku master`
+  - `heroku logs`
+  - `heroku run rake db:migrate`
+- Disabling rails security measures
+  - Not recommended
+  - Needed for group project
+- Installing PostgreSQL and the pg gem
+- Creating additional routes
+- Going from desired feature to solution
+	- Break down the problem
+	- What do I want?
+	- Could I do that in just pry?
+	- How do I expose that data in a Route
+	- What format is required?
+	- What should the response code be?
