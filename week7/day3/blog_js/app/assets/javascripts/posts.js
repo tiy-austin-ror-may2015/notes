801ad0d00e95a03dcee9e16d8e3fb3e1ea56c613
Justin Herrick <justin@justinherrick.com>
2015-06-17 16:07:03 -0500

    Update posts.js

--- week7/day3/blog_js/app/assets/javascripts/posts.js
+++ week7/day3/blog_js/app/assets/javascripts/posts.js
@@ -1,14 +1,15 @@
 /* global $ */
-//title present
-//body min 5
-$('#post_title').on('keyup', function () {
-  var title_value = $(this).val();
-  var error_elem = $(this).parent().find('.errors');
 
-  if (title_value.length < 1) {
-    error_elem.text('Title is required');
+// New Post Title Validation
+$('#post_title').on('keyup', function () { // 'keyup' is an event that fires after every keystroke
+  // 'this' is the input element with the id of '#post_title'
+  var title_value = $(this).val(); //Get the value out of the input element
+  var error_elem = $(this).parent().find('.errors'); //Find the closest element that has the class 'errors'
+
+  if (title_value.length < 1) {  //If the string inside of the input is less than 1 char, we assume its blank
+    error_elem.text('Title is required'); //Change the text of the error field to say what is wrong
   } else {
-    error_elem.text('');
+    error_elem.text(''); // If there is text in the title input field, remove the error text.
   }
 });
 
